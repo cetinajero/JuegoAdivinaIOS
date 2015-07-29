b2ec9c2f5f135557463a6f0d209009e4ffaaa38d
Edgar Tinajero <cetinajero@gmail.com>
2015-07-29 15:16:53 -0600

    Initial Commit

---
+++ JuegoAdivinaIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuegoAdivinaIOS
+//
+//  Created by Edgar Tinajero on 7/29/15.
+//  Copyright (c) 2015 PV Comunicaciones S.A de C.V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
